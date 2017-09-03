480e55e67aa34e0854c347c9c6bab427694c5880
mjms3 <shepherd.max@gmail.com>
2017-09-03 10:47:46 +0000

    Use xml unmarshal to extract the innerxml from a fixed input field

--- extractnodes/extractnodes.go
+++ extractnodes/extractnodes.go
@@ -1,6 +1,29 @@
 package extractnodes
 
-func ExtractNodes(inputXmlString string) []string {
-	output := []string{inputXmlString}
-	return output
+import (
+	"encoding/xml"
+	"fmt"
+)
+
+func ExtractNodes(inputXmlString string) []string  {
+	type InnerXml struct {
+		Content string `xml:",innerxml"`
+	}
+
+	type NodeList struct {
+		Tag    []InnerXml     `xml:"A"`
+	}
+
+	result := NodeList{}
+	err := xml.Unmarshal([]byte(inputXmlString), &result)
+	if err != nil {
+		fmt.Printf("error: %s\n", err)
+	}
+
+	innerXmlList := make([]string,len(result.Tag))
+	for idx := range result.Tag {
+		innerXmlList[idx] = result.Tag[idx].Content
+	}
+
+	return innerXmlList
 }
--- extractnodes/extractnodes_test.go
+++ extractnodes/extractnodes_test.go
@@ -1,11 +1,13 @@
 package extractnodes
 
-import "testing"
-import "reflect"
+import (
+	"testing"
+	"reflect"
+)
 
 func TestExtractNodes(t *testing.T){
-	inString := "<a>Test</a>"
-	outArray := []string{inString}
+	inString := `<data><A>Test1</A><A>Test2</A></data>`
+	outArray := []string{`Test1`,`Test2`}
 	extractedNodes := ExtractNodes(inString)
 	if !reflect.DeepEqual(outArray,extractedNodes) {
 		t.Errorf("ExtractNodes with input %s returned %s, expected: %s", inString, extractedNodes, outArray)
